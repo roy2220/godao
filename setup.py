47ba746fbfc83ff5162736be443f0d3f45d19020
Roy O'Young <roy2220@outlook.com>
2019-07-22 02:48:10 +0800

    Add support for directive line continuation

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name="godao",
     version="0.1.0",
-    description="DAO Generator for Go",
+    description="DAO Generation for Go",
     scripts=["bin/godao"],
     install_requires=["mako", "pyyaml", "yamlordereddictloader"],
 )
